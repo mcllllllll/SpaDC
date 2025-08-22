29dbbf5f6c6c95e733d30c9bf4566e5c797c01d6
mcllllllll <614396628@qq.com>
2025-08-22 13:48:55 +0800

    auto update on 2025-08-22 13:48:55

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+__lib_name__ = "SpaDC"
+__lib_version__ = "0.1.0"
+__description__ = "SpaDC"
+__url__ = "https://github.com/mcllllllll/SpaDC"
+__author__ = "Chuanlong Ma"
+__author_email__ = "614396628@qq.com"
+__license__ = "MIT"
+__keywords__ = ["spatial epigenomics"]
+__requires__ = []
+
+setup(
+    name = __lib_name__,
+    version = __lib_version__,
+    description = __description__,
+    url = __url__,
+    author = __author__,
+    author_email = __author_email__,
+    license = __license__,
+    packages = ['SpaDC'],
+    install_requires = __requires__,
+    zip_safe = False,
+    include_package_data = True,
+)
