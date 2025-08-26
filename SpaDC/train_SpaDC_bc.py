7d1c99bc77501c903739fbddbb5394299f54cbf3
mcllllllll <614396628@qq.com>
2025-08-26 12:13:09 +0800

    auto update on 2025-08-26 12:13:09

--- SpaDC/train_SpaDC_bc.py
+++ SpaDC/train_SpaDC_bc.py
@@ -7,19 +7,19 @@ import numpy as np
 from tqdm import tqdm
 from scipy.sparse import coo_matrix
     
-def train_SpaDC_bc(adata, ATAC, H3K27ac, seq, hidden_size=32, n_epochs1=100, n_epochs2 = 100, 
+def train_SpaDC_bc(integrate, adata1, adata2, seq, hidden_size=32, n_epochs1=100, n_epochs2 = 100, 
                        batch_size=1024, lr=1e-2, lambda1=1e-8, lambda2 = 1e-9, random_seed=40, 
                        save_model=False, device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')):
     # seed_everything()
     set_seed(random_seed)
 
-    matrix = torch.FloatTensor(adata.X.todense().transpose()) 
+    matrix = torch.FloatTensor(integrate.X.todense().transpose()) 
 
-    spatial1 = construct_graph_by_coordinate(ATAC.obsm['spatial'], n_neighbors=6)
-    spatial2 = construct_graph_by_coordinate(H3K27ac.obsm['spatial'], n_neighbors=6)
+    spatial1 = construct_graph_by_coordinate(adata1.obsm['spatial'], n_neighbors=6)
+    spatial2 = construct_graph_by_coordinate(adata2.obsm['spatial'], n_neighbors=6)
 
-    adj1 = coo_matrix((spatial1['value'], (spatial1['x'],spatial1['y'])), shape=(ATAC.n_obs,ATAC.n_obs),dtype=int)
-    adj2 = coo_matrix((spatial2['value'], (spatial2['x'],spatial2['y'])), shape=(H3K27ac.n_obs,H3K27ac.n_obs),dtype=int)
+    adj1 = coo_matrix((spatial1['value'], (spatial1['x'],spatial1['y'])), shape=(adata1.n_obs,adata1.n_obs),dtype=int)
+    adj2 = coo_matrix((spatial2['value'], (spatial2['x'],spatial2['y'])), shape=(adata2.n_obs,adata2.n_obs),dtype=int)
     adj1 = torch.FloatTensor(adj1.todense()).to(device)
     adj2 = torch.FloatTensor(adj2.todense()) .to(device)
     
@@ -35,7 +35,7 @@ def train_SpaDC_bc(adata, ATAC, H3K27ac, seq, hidden_size=32, n_epochs1=100, n_e
 
     train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
-    model = SpaDC(n_cells=adata.n_obs, hidden_size=hidden_size).to(device)
+    model = SpaDC(n_cells=integrate.n_obs, hidden_size=hidden_size).to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
@@ -55,8 +55,8 @@ def train_SpaDC_bc(adata, ATAC, H3K27ac, seq, hidden_size=32, n_epochs1=100, n_e
       
             for name,p in model.named_parameters():
                 if 'cell_embedding.weight' in name:
-                    lap_loss1 = lap_reg(adj1, p[0:ATAC.n_obs, :])  
-                    lap_loss2 = lap_reg(adj2, p[ATAC.n_obs:ATAC.n_obs+H3K27ac.n_obs, :])            
+                    lap_loss1 = lap_reg(adj1, p[0:adata1.n_obs, :])  
+                    lap_loss2 = lap_reg(adj2, p[adata1.n_obs:adata1.n_obs+adata2.n_obs, :])            
 
             loss = bce_loss + lambda1 * (lap_loss1 + lap_loss2)
 
@@ -76,10 +76,10 @@ def train_SpaDC_bc(adata, ATAC, H3K27ac, seq, hidden_size=32, n_epochs1=100, n_e
 
     cell_embedding = model.get_embedding().to('cpu').detach().numpy()  
     
-    adata.obsm['SpaDC'] = cell_embedding
+    integrate.obsm['SpaDC'] = cell_embedding
     
     # unique index
-    adata.obs.index = [str(i) for i in np.arange(adata.n_obs)]
+    integrate.obs.index = [str(i) for i in np.arange(integrate.n_obs)]
 
     anchor_ind = []
     positive_ind = []
@@ -89,8 +89,8 @@ def train_SpaDC_bc(adata, ATAC, H3K27ac, seq, hidden_size=32, n_epochs1=100, n_e
         if epoch == n_epochs1+1:
             # If knn_neigh>1, points in one slice may have multiple MNN points in another slice.
             # not all points have MNN achors
-            mnn_dict = create_dictionary_mnn(adata, use_rep='SpaDC', batch_name='batch', k=50, verbose=0)
-            knn_dict = create_dictionary_knn(adata, use_rep='SpaDC', batch_name='batch')
+            mnn_dict = create_dictionary_mnn(integrate, use_rep='SpaDC', batch_name='batch', k=50, verbose=0)
+            knn_dict = create_dictionary_knn(integrate, use_rep='SpaDC', batch_name='batch')
 
             for batch_pair in mnn_dict.keys():  # pairwise compare for multiple batches
                 anchor_list = []
@@ -102,7 +102,7 @@ def train_SpaDC_bc(adata, ATAC, H3K27ac, seq, hidden_size=32, n_epochs1=100, n_e
                     positive_list.append(positive_spot)
                     negative_list.append(knn_dict[anchor])  # select the first knn spot 
 
-                batch_as_dict = dict(zip(list(adata.obs_names), range(0, adata.shape[0])))
+                batch_as_dict = dict(zip(list(integrate.obs_names), range(0, integrate.shape[0])))
                 anchor_ind = np.append(anchor_ind, list(map(lambda _: batch_as_dict[_], anchor_list)))
                 positive_ind = np.append(positive_ind, list(map(lambda _: batch_as_dict[_], positive_list)))
                 negative_ind = np.append(negative_ind, list(map(lambda _: batch_as_dict[_], negative_list)))
@@ -124,8 +124,8 @@ def train_SpaDC_bc(adata, ATAC, H3K27ac, seq, hidden_size=32, n_epochs1=100, n_e
 
             for name,p in model.named_parameters():
                 if 'cell_embedding.weight' in name:
-                    lap_loss1 = lap_reg(adj1, p[0:ATAC.n_obs, :])  
-                    lap_loss2 = lap_reg(adj2, p[ATAC.n_obs:ATAC.n_obs+H3K27ac.n_obs, :]) 
+                    lap_loss1 = lap_reg(adj1, p[0:adata1.n_obs, :])  
+                    lap_loss2 = lap_reg(adj2, p[adata1.n_obs:adata1.n_obs+adata2.n_obs, :]) 
                     anchor_arr = p[anchor_ind,]
                     positive_arr = p[positive_ind,]
                     negative_arr = p[negative_ind,]   
@@ -151,8 +151,8 @@ def train_SpaDC_bc(adata, ATAC, H3K27ac, seq, hidden_size=32, n_epochs1=100, n_e
     if save_model == True:
         torch.save(model.state_dict(), 'result/model.pt')     
 
-    adata.obsm['SpaDC_bc'] = model.get_embedding().to('cpu').detach().numpy()  
-    return adata
+    integrate.obsm['SpaDC_bc'] = model.get_embedding().to('cpu').detach().numpy()  
+    return integrate
 
 
 
